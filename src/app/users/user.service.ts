d8239fe1f2759ad8af5a83fec427bfed59405aa8
Henrique Anraki <henrique_anraki@outlook.com>
2021-10-20 18:34:48 -0300

    Finished user creation

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,12 +1,37 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppConfigService } from '../shared/services/app-config.service';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private appConfigService: AppConfigService,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
-  login(credentials: Object) {
-    console.log()
-    return false
+  login(credentials: any) {
+    // Como/onde criar constantes de configuração baseadas no enviroment?
+    let endpoint = this.appConfigService.apiEndpoint
+    
+    console.log(endpoint)
+    
+    return this.http.post(`${endpoint}/user`, JSON.stringify({
+      emailAddress: credentials.email,
+      password: credentials.password
+    }))
+      .subscribe( res => {
+        console.log('resposta do post: ')
+        console.log(res)
+        alert("Conta criada! Já pode usar a sua agenda!")
+        this.router.navigate(['/login'])
+      },
+      (error: HttpErrorResponse) => {
+        // tratar melhor os erros
+        console.log(error)
+        alert(error.error.text ?? error.error)
+      });
   }
 }
