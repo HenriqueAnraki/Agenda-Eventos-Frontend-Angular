6e338bb9c0f42d1af5b11e5e95b8ca749cb55cd4
Henrique Anraki <henrique_anraki@outlook.com>
2021-10-20 16:05:04 -0300

    Highlight invalid fields after form submitting

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,10 +21,28 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     console.log(this.form)
+
+    if (this.form.valid) {
+      console.log('tudo certo!')
+      // fazer login
+    } else {
+      console.log('Form Inválido!')
+      this.verifyForm(this.form)
+    }
+  }
+  
+  // colocar em um serviço compartilhado
+  verifyForm(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach( field => {
+      const control = formGroup.get(field);
+      control?.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.verifyForm(control);
+      }
+    });
   }
 
-
-  // colocar em um serviço compartilhado
   isFieldValid(field: any): boolean {
     return !this.form.get(field)?.valid && (!!this.form.get(field)?.touched || !!this.form.get(field)?.dirty)
   }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { LoginService } from './login.service';
 
 
 
@@ -14,6 +15,9 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     ReactiveFormsModule,
     SharedModule
+  ],
+  providers: [
+    LoginService
   ]
 })
 export class LoginModule { }
